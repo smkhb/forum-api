22149bfaa95976296c18f3a1b195f6ca5c6c7861
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-12-04 10:10:45 -0300

    Organizando propriedades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 interface AnswerProps {
+  authorID: UniqueEntityID
+  questionID: UniqueEntityID
   content: string
-  authorID: string
-  questionID: string
+  createdAt: Date
+  updatedAt?: Date
 }
 export class Answer extends Entity<AnswerProps> {
   get content() {
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 interface QuestionProps {
+  authorID: UniqueEntityID
+  bestAnswerID?: UniqueEntityID
   title: string
   content: string 
   slug: Slug
-  authorID: string 
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question  extends Entity<QuestionProps> {
