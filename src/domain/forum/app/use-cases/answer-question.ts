d1ad9dc3827bfde9f19f1106c132dcc670eeef51
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-12-04 14:48:52 -0300

    reorganizando estrutura de pastas

--- src/domain/forum/app/use-cases/answer-question.ts
+++ src/domain/forum/app/use-cases/answer-question.ts
@@ -1,6 +1,6 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import { Answer } from '../entities/answer'
-import { AnswerRepository } from '../repositories/answer-repository'
+import { AnswerRepository } from '@/domain/forum/app/repositories/answer-repository'
+import { Answer } from '../../enterprice/entities/answer'
 
 interface AnswerQuestionUseCaseResquest {
   instructorID: string
