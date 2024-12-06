5b0de5af8864c6057307209a56fda4302186750f
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-12-06 10:54:14 -0300

    Faker Js para Testes

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   Question,
@@ -5,14 +6,20 @@ import {
 } from '@/domain/forum/enterprice/entities/question'
 import { Slug } from '@/domain/forum/enterprice/entities/value-objects/slug'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorID: new UniqueEntityID(),
-    title: 'Example Question',
-    slug: Slug.create('example-question'),
-    content: 'Example content',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  ID?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorID: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      slug: Slug.create(faker.lorem.slug()),
+      ...override,
+    },
+    ID,
+  )
 
   return question
 }
